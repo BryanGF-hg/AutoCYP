64686bb28640a3997d66790ef9f0feead3db90a2
Bryan <bryan.alexander.glot.fong@alu.ceacfp.es>
2025-10-30 08:50:02 +0100

    AutoCYP v2.0.2

--- AutoCYP.py
+++ AutoCYP.py
@@ -1,11 +1,16 @@
-# Programa Semi-Automático de Copiar y Pegar v0.2
+# Programa Semi-Automático de Copiar y Pegar v0.2.2
 # Hecho por Bryan Glot Fong - 1 DAM con ayuda del profesorado
 
-# v0.2: Mejoras gramáticales hechas,    Función ComillasCerradas creada para usarla,   Función borrar_espacios creada para usarla
+# v0.2.2: Varias etiquetas creadas como notas para el usuario que usa el programa, nombres de funciones cambiados, BOTON salir creado
+# v0.2.1: Mejoras gramáticales hechas,    Función ComillasCerradas creada para usarla,   Función borrar_espacios creada para usarla
 
 #RECOMENDACIONES:
 #OPCIONAL--Cambiar "datos-AutoCYP.txt" a otro "archivo.txt" si quieres cambiar el archivo(solo se encuentra dentro de las funciones leer y escribir)
-#Crear un boton eliminar
+
+#Escribir permite crear la estructura predefinida
+#ComillasCerradas permite crear 2 lineas de comillas cerradas
+
+
 
 
 import tkinter as tk
@@ -48,16 +53,24 @@ def comillascerradas():
  archivo.write("```\n"+""+"```\n")
  archivo.close()
 
-
-
 #función borrar_espacios
-def borrar_espacios():
+def quitar_espacios():
  archivo = open("datos-AutoCYP.txt",'a')
- archivo.truncate(5000)
+ archivo.truncate(500)
  archivo.close()
  #Permite leer inmediamente después de usar la función borrar_espacios
  archivo = open("datos-AutoCYP.txt","r")
  print("\n"+archivo.read())
+
+
+#funcion eliminar
+def eliminar():
+ archivo = open("datos-AutoCYP.txt","w")
+ archivo.write("\n")
+ archivo.close()
+
+###Titulo###
+tk.Label(marco,text="[[AutoCYP v0.2.2]]").pack(padx=10,pady=10)
 
 #1.-Introducción
 tk.Label(marco,text="Introduce el contenido de      1.-Introducción").pack(padx=10,pady=10)
@@ -86,14 +99,17 @@ codigo.pack(padx=10,pady=10)
 
 
 #BOTON (llamada a la funcion escribir)
-tk.Button(marco,text="Pulsa para escribir",command = escribir).pack(padx=10,pady=10)
+tk.Button(marco,text="Pulsa para escribir [contenido]",command = escribir).pack(padx=10,pady=10)
 #BOTON (llamada a la funcion comillascerradas)
 tk.Button(marco,text="Pulsa para crear ComillasCerradas",command = comillascerradas).pack(padx=10,pady=10)
-#BOTON (llamada a la funcion borrar_espacios)
-tk.Button(marco,text="Pulsa para borrar espacio",command = borrar_espacios).pack(padx=10,pady=10)
+#BOTON (llamada a la funcion quitar_espacios)
+tk.Button(marco,text="Pulsa para quitar espacio",command = quitar_espacios).pack(padx=10,pady=10)
 #BOTON (llamada a la funcion leer)
 tk.Button(marco,text="---------Pulsa para leer---------",command = leer).pack(padx=10,pady=10)
+#BOTON (llamada a la funcion eliminar)
+tk.Button(marco,text="---------Pulsa para crear espacio en blanco---------",command = eliminar).pack(padx=10,pady=10)
 
+tk.Button(marco, text="Salir", command=ventana.destroy).pack(padx=10,pady=10)
 
 
 
